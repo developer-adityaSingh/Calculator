6b2fb56242aa96e189cc0deefb586e756c66391a
Aditya Singh <1211821022@ngpp.ac.in>
2024-07-21 20:56:53 +0530

    added eventListner for calculation and also added responsive design

--- script.js
+++ script.js
@@ -1,142 +1,76 @@
-let add = (num1,num2) => (num1+num2)
-let sub = (num1,num2) => (num1-num2)
-let mul = (num1,num2) => (num1*num2)
-let div = (num1,num2) => (num1/num2)
-var inputValue = document.getElementById("inputArea").value
-var finalAnswer=0
-document.getElementById('allClear').addEventListener('click',()=>{
-    console.log("AllClear was clicked")
-    document.getElementById("inputArea").value= ""
-})
+let buttons= document.querySelector('.buttons')
 
-document.getElementById('delete').addEventListener('click',()=>{
-    console.log("delete was clicked")
-    document.getElementById("inputArea").value= ""
-})
+// onScreenValue= document.getElementById("inputArea")
+buttons.addEventListener("click", (e)=>{
+    let previousValue=0
+    onScreenInput= document.getElementById("inputArea") 
+    onScreenValue= onScreenInput.value
+    e.stopPropagation()
+    idNode= e.target.id;
+    if (idNode == "one") {
+        previousValue= 1
+        document.getElementById("inputArea").value= `${onScreenValue}1`
 
-document.getElementById('percentage').addEventListener('click',()=>{
-    console.log("percentage was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}%`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('divide').addEventListener('click',()=>{
-    console.log("divide was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}/`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('seven').addEventListener('click',()=>{
-    console.log("seven was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}7`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('eight').addEventListener('click',()=>{
-    console.log("eight was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}8`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('nine').addEventListener('click',()=>{
-    console.log("nine was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}9`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('multiply').addEventListener('click',()=>{
-    console.log("multiply was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}*`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('four').addEventListener('click',()=>{
-    console.log("four was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}4`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('five').addEventListener('click',()=>{
-    console.log("five was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}5`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('six').addEventListener('click',()=>{
-    console.log("six was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}6`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('subtract').addEventListener('click',()=>{
-    console.log("subtract was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}-`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('one').addEventListener('click',()=>{
-    console.log("one was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}1`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('two').addEventListener('click',()=>{
-    console.log("two was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}2`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('three').addEventListener('click',()=>{
-    console.log("three was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}3`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('add').addEventListener('click',()=>{
-    console.log("add was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}+`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('dZero').addEventListener('click',()=>{
-    console.log("dZero was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}00`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('zero').addEventListener('click',()=>{
-    console.log("zero was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}0`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('decimal').addEventListener('click',()=>{
-    console.log("decimal was clicked")
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue}.`
-    inputValue= document.getElementById("inputArea").value
-})
-
-document.getElementById('equalsTO').addEventListener('click',()=>{
-    console.log("equalsTO was clicked")
-    console.log(inputValue)
-    finalAnswer= eval(`${inputValue}`)
-    inputValue= document.getElementById("inputArea").value
-    document.getElementById("inputArea").value= `${inputValue} = ${finalAnswer}`
+    }
+    else if(idNode == "two"){
+        document.getElementById("inputArea").value= `${onScreenValue}2`
+    }
+    else if(idNode == "three"){
+        document.getElementById("inputArea").value= `${onScreenValue}3`
+    }
+    else if(idNode == "four"){
+        document.getElementById("inputArea").value= `${onScreenValue}4`
+    }
+    else if(idNode == "five"){
+        document.getElementById("inputArea").value= `${onScreenValue}5`
+    }
+    else if(idNode == "six"){
+        document.getElementById("inputArea").value= `${onScreenValue}6`
+    }
+    else if(idNode == "seven"){
+        document.getElementById("inputArea").value= `${onScreenValue}7`
+    }
+    else if(idNode == "eight"){
+        document.getElementById("inputArea").value= `${onScreenValue}8`
+    }
+    else if(idNode == "nine"){
+        document.getElementById("inputArea").value= `${onScreenValue}9`
+    }
+    else if(idNode == "divide"){
+        document.getElementById("inputArea").value= `${onScreenValue}/`
+    }
+    else if(idNode == "multiply"){
+        document.getElementById("inputArea").value= `${onScreenValue}*`
+    }
+    else if(idNode == "add"){
+        document.getElementById("inputArea").value= `${onScreenValue}+`
+    }
+    else if(idNode == "subtract"){
+        document.getElementById("inputArea").value= `${onScreenValue}-`
+    }
+    else if(idNode == "dZero"){
+        document.getElementById("inputArea").value= `${onScreenValue}00`
+    }
+    else if(idNode == "zero"){
+        document.getElementById("inputArea").value= `${onScreenValue}0`
+    }
+    else if(idNode == "equalsTO"){
+        document.getElementById("inputArea").value= `${onScreenValue} = ${eval(onScreenValue)}`
+    }
+    else if(idNode == "decimal"){
+        document.getElementById("inputArea").value= `${onScreenValue}.`
+    }
+    else if(idNode == "allClear"){
+        document.getElementById("inputArea").value= ""
+    }
+    else if(idNode == "delete"){
+        let newArr= Array.from(onScreenValue)
+        newArr.pop()
+        let updatedNumber= String(newArr.join(""))
+        document.getElementById("inputArea").value= `${updatedNumber}`
+    }
+    else if(idNode == "percentage"){
+        document.getElementById("inputArea").value= `${onScreenValue}%`
+    }
+    
 })
